235f076f9a750388aaadad065c42012ee7209106
FallenTeen <vlamingvlaming0@gmail.com>
2026-09-20 16:32:03 +0700

    build: build new feature

--- public/build/assets/Layout-CFST0Y03.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./app-h39ue2vl.js";import{t}from"./AuthenticatedLayout-j4wfVmUZ.js";var n=e();function r({header:e,children:r}){return(0,n.jsx)(t,{header:e,children:r})}export{r as t};
